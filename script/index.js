85f71ea410078458e3069bae4440041fd7c4f282
Yohran Kallahari <yohranv@gmail.com>
2023-01-25 18:16:48 -0300

    Atualização in (img, html, css, script)

--- script/index.js
+++ script/index.js
@@ -1,12 +1,13 @@
+let p = document.getElementsByClassName("name")[0]
 let lista = document.getElementById("lista")
 
-conteudo = ""
+p.innerHTML = "Yohran_Kallahari"
 
+conteudo = ""
     conteudo = "<a href='../HTML/sobre.html'><li>" + "Sobre Mim" + "</li></a>"
     conteudo += "<a href='../Yohran_VenâncioCamiloMorais_Currículo.pdf'><li>" + "Currículo" + "</li></a>"
     conteudo += "<a href='../projetos/Sr. Mackfly/HTML/index.html'><li>" + "Projeto" + "</li></a>"
     conteudo += "<a href='https://www.linkedin.com/in/yohran-ven%C3%A2ncio-96017b120/'><li>" + "Linkedin" + "</li></a>"
     conteudo += "<a href='https://github.com/yohran1'><li>" + "GitHub" + "</li></a>"
-
 
 lista.innerHTML = conteudo
