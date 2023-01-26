77a259320e943356c78cfe4d37a12a71a29be684
Yohran Kallahari <yohranv@gmail.com>
2023-01-26 17:24:26 -0300

    Atualização no style para telas/monitores menores

---
+++ projetos/Sr. Mackfly/SCRIPT/script.js
@@ -0,0 +1,11 @@
+let lista = document.getElementById("lista")
+
+conteudo=""
+
+conteudo = "<a href='#'><li>" + "Página Inicial" + "</li></a>"
+conteudo += "<a href='#'><li>" + "Peça Online" + "</li></a>"
+conteudo += "<a href='#'><li>" + "Contato" + "</li></a>"
+conteudo += "<a href='#'><li>" + "Sobre" + "</li></a>"
+conteudo += "<a href='#'><li>" + "Feedback" + "</li></a>"
+
+lista.innerHTML = conteudo
