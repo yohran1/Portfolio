abb42b0eb1e51b775cdceeeb86d530827bf6ea03
Yohran Kallahari <yohranv@gmail.com>
2023-10-23 21:38:00 -0300

    Criação do projeto!

---
+++ src/components/pages/Sobre.js
@@ -0,0 +1,11 @@
+import Container from "../layout/Container";
+
+
+export default function Sobre(){
+
+    return(
+        <Container>
+            <h1>Sobre</h1>
+        </Container>
+    )
+}
---
+++ src/components/project/SrMackfly.js
@@ -0,0 +1,10 @@
+import Container from '../layout/Container'
+
+export default function SrMackfly(){
+
+    return(
+        <Container>
+            <h1>Projeto</h1>
+        </Container>
+    )
+}
