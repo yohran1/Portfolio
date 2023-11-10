cc616ca8d36f5faf8a931620d479faa280da03aa
Yohran Kallahari <yohranv@gmail.com>
2023-11-09 22:05:46 -0300

    Ajuste

--- src/components/project/components/NavBar.js
+++ src/components/project/components/NavBar.js
@@ -5,20 +5,26 @@ import { Link } from 'react-router-dom'
 export default function NavBar(){
 
     return(
-        <div className={style.navBar}>
-
-            <Link to='/projeto/srmackfly' className={style.logo}>
+        <nav className={style.navBar}>
+            <div className={style.logo}>
+            <Link to='/projeto/srmackfly'>
                 <img src={icon} alt={icon} />
                 <p>Bolos de Casamento Por Sr-MackFly</p>
             </Link>
+            </div>
 
-            <ul>
-                <li><Link to='/projeto/srmackfly'>Página Inicial</Link></li>
-                <li><Link to='/projeto/srmackfly'>Peça Online</Link></li>
-                <li><Link to='/projeto/srmackfly'>Contato</Link></li>
-                <li><Link to='/projeto/srmackfly'>Sobre</Link></li>
+            <div className={style.nav_list}>
+                <ul>
+                    <li><Link to='/projeto/srmackfly'>Página Inicial</Link></li>
+                    <li><Link to='/projeto/srmackfly'>Peça Online</Link></li>
+                    <li><Link to='/projeto/srmackfly'>Contato</Link></li>
+                    <li><Link to='/projeto/srmackfly'>Sobre</Link></li>
+                </ul>
+            </div>
+            
+            <div className={style.login_btn}>
                 <a href='http://localhost:3000/projeto/login'><button>Login</button></a>
-            </ul>
-        </div>
+            </div>
+        </nav>
     )
 }
