06705b801da189989618fe52dd948138e8eb4f74
Yohran Kallahari <yohranv@gmail.com>
2023-11-15 17:30:49 -0300

    Conteúdo com links a páginas sobre o projeto

--- src/components/project/components/pages/SrMackfly.js
+++ src/components/project/components/pages/SrMackfly.js
@@ -3,7 +3,7 @@ import Container from '../../../layout/Container'
 import Footer from '../../../layout/Footer'
 import NavBar from '../NavBar'
 import cakes_1 from '../../img/cakes/bolo-layout-4.jpg'
-import cakes_2 from '../../img/cakes/bolo-branco.webp'
+import CakesSection from '../CakesSection'
 
 export default function SrMackfly(){
 
@@ -15,16 +15,8 @@ export default function SrMackfly(){
                     <img src={cakes_1} alt={cakes_1} />
                 </div>
                 
-                <div className={style.cakes_info}>
-                    <div className={style.info}>
-                        <h2>MEUS BOLOS SÃO CONFECCIONADOS COM AMOR</h2>
-                        <p>Sou um parágrafo. Clique aqui para editar e adicionar o seu próprio texto. É fácil! Basta clicar em “Editar Texto” ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sou um ótimo espaço para você contar sua história.</p>
-                    </div>
-
-                    <div className={style.img_cake}>
-                        <img src={cakes_2} alt={cakes_2} />
-                    </div>
-                </div>
+                <CakesSection />
+                
 
                 
             </div>
